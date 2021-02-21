f38522e538003edb7c29c8cda04efd4b63834300
ncavchl <ncavchl@ajou.ac.kr>
2021-02-22 01:23:26 +0900

    하단 네비게이션

---
+++ helloworld/lib/widget/bottom_bar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+//상태관리 필요없는 위젯 - 바텀바 - stateless
+class Bottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color:Colors.black,
+        child:Container(
+            height:50,
+            child:TabBar(
+                labelColor:Colors.white,
+                unselectedLabelColor: Colors.white60,
+                indicatorColor: Colors.transparent,
+                tabs:<Widget>[
+                  Tab(
+                      icon:Icon(
+                        Icons.home,
+                        size:18,
+                      ),
+                      child:Text(
+                        '홈',
+                        style:TextStyle(fontSize:9),
+                      )
+                  ),
+                  Tab(
+                      icon:Icon(
+                        Icons.search,
+                        size:18,
+                      ),
+                      child:Text(
+                        '검색',
+                        style:TextStyle(fontSize:9),
+                      )
+                  ),
+                  Tab(
+                      icon:Icon(
+                        Icons.save_alt,
+                        size:18,
+                      ),
+                      child:Text(
+                        '저장한 콘텐츠 목록',
+                        style:TextStyle(fontSize:9),
+                      )
+                  ),
+                  Tab(
+                      icon:Icon(
+                        Icons.list,
+                        size:18,
+                      ),
+                      child:Text(
+                        '더보기',
+                        style:TextStyle(fontSize:9),
+                      )
+                  ),
+                ]
+            )
+        )
+    );
+
+  }
+}
