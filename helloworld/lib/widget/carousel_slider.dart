edb61abb32d472da27fb58fe5fac7790577f6f35
ncavchl <ncavchl@ajou.ac.kr>
2021-03-02 23:16:24 +0900

    ㅇ캐러셋

--- helloworld/lib/widget/carousel_slider.dart
+++ helloworld/lib/widget/carousel_slider.dart
@@ -14,14 +14,41 @@ class _CarouselImageState extends State<CarouselImage> {
   List<Widget> images;
   List<String> kyewords;
   List<bool> likes;
+  int _currentPage = 0;
+  String _currentKeyword;
 
   @override 
   void initState() {
     super.initState();
+    movies = widget.movies;
+    images = movies.map((m) => Image.asset('./images/' + m.poster)).toList();
+    keywords = movies.map((m) => m.keyword).toList();
+    likes = movies.map((m) => m.like).toList();
+    _currentKeyword = keywords[0];
   }
 
   @override 
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: <Widget> [
+          Container(
+            padding: EdgeInsets.all(20),
+          ),
+          CarouselSlider(
+            items: images, 
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+                _currentKeyword = kyewords[_currentPage];
+              });
+            },
+          },    
+          Container(
+            child: Text(_currentKeyword),
+          ),
+        ],
+      },
+    );
   }
 }
