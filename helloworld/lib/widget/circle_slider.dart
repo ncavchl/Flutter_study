dea337b40b38b33b6d55c685605ec91e9efc9531
ncavchl <ncavchl@hyundai-autoever.com>
2021-03-23 23:49:27 +0900

    box, circle slider

---
+++ helloworld/lib/widget/circle_slider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/model/model_movie.dart';
+
+class CirclesSlider extends StatelessWidget {
+  final List<Movie> movies;
+  CirclesSlider({this.movies});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(7),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+      )
+    );
+  }
+
+}
